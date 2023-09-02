d10c8f1eb078e4e56e424ecb3e79084445ed29a2
Artem Ivanov <ivanov@hostco.ru>
2023-09-02 21:54:54 +0500

    add pagination, need add crud

--- src/components/Panel/Panel.tsx
+++ src/components/Panel/Panel.tsx
@@ -16,7 +16,7 @@ export const Panel = () => {
 	}
 
 	return (
-		<Paper elevation={3} sx={{ padding: '25px 30px', display: 'flex', justifyContent: 'space-between', alignContent: 'center', gap: 5 }}>
+		<Paper elevation={3} sx={{ marginBottom: 2, padding: '25px 30px', display: 'flex', justifyContent: 'space-between', alignContent: 'center', gap: 5 }}>
 			<TextField value={value} onChange={onChange} label='Todo Text' fullWidth />
 			<Button variant='outlined' onClick={onClick}>ADD</Button>
 		</Paper>
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -2,27 +2,60 @@ import React from 'react'
 
 import { TodoItem } from './TodoItem/TodoItem'
 
-import { TableContainer, Table, Paper, TableHead, TableRow, TableCell, TableBody } from '@mui/material'
+import { Box, TableContainer, Table, Paper, TableHead, TableRow, TableCell, TableBody, TableFooter, TablePagination } from '@mui/material'
 
 const list = [
 	{ id: 0, desc: 'sample 1', checked: false },
 	{ id: 1, desc: 'sample 2', checked: false },
 	{ id: 2, desc: 'sample 3', checked: true },
 	{ id: 3, desc: 'sample 4', checked: false },
+	{ id: 4, desc: 'sample 5', checked: false },
+	{ id: 5, desc: 'sample 6', checked: true },
+	{ id: 6, desc: 'sample 7', checked: true },
 ]
 
-export const TodoList = () => <TableContainer component={Paper}>
-	<Table sx={{ marginTop: 2 }}>
-	<TableHead>
-          <TableRow>
-            <TableCell align="left">Id</TableCell>
-            <TableCell align="left">Name</TableCell>
-            <TableCell align="left">Checked</TableCell>
-            <TableCell align="left" colSpan={2}>Actions</TableCell>
-          </TableRow>
-        </TableHead>
-		<TableBody>
-			{list.map((item, index) => <TodoItem key={index} todo={item} />)}
-		</TableBody>
-	</Table>
-</TableContainer>
+export const TodoList = () => {
+
+	const [page, setPage] = React.useState(0);
+	const [rowsPerPage, setRowsPerPage] = React.useState(5);
+	
+	const handlePageChange = (event: any, newPage: any) => {
+		setPage(newPage);
+	}
+
+	const handleRowsPerPageChange = (event: any) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    }
+
+	return (
+		<Box>
+			<TableContainer component={Paper}>
+				<Table sx={{ marginTop: 2 }}>
+					<TableHead>
+						<TableRow>
+							<TableCell align="left">Id</TableCell>
+							<TableCell align="left">Name</TableCell>
+							<TableCell align="left">Checked</TableCell>
+							<TableCell align="left" colSpan={2}>Actions</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{list
+							.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+							.map((item, index) => <TodoItem key={index} todo={item} />)}
+					</TableBody>
+				</Table>
+			</TableContainer>
+			<TablePagination
+				page={page}
+				rowsPerPage={rowsPerPage}
+				count={list.length}
+				rowsPerPageOptions={[5, 10, 25]}
+				component="div"
+				onPageChange={handlePageChange}
+				onRowsPerPageChange={handleRowsPerPageChange}
+			/>
+		</Box>
+	)
+}
